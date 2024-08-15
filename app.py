20d5b3c950ebbe9ac858552a718d79cc120c7621
spoonwep <i@sell.moe>
2024-08-15 12:14:14 +0800

    add cog files

--- app.py
+++ app.py
@@ -1,86 +1,42 @@
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
+import torch
+import cv2
+import numpy as np
+from bottle import get, post, request, run, response
 
-# use bfloat16 for the entire notebook
-torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
-
-if torch.cuda.get_device_properties(0).major >= 8:
-    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
-
-def show_mask(mask, ax, random_color=False, borders = True):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
-    mask = mask.astype(np.uint8)
-    mask_image =  mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    if borders:
-        import cv2
-        contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE) 
-        # Try to smooth contours
-        contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
-        mask_image = cv2.drawContours(mask_image, contours, -1, (1, 1, 1, 0.5), thickness=2) 
-    ax.imshow(mask_image)
-
-def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-
-def show_box(box, ax):
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
-
-def show_masks(image, masks, scores, point_coords=None, box_coords=None, input_labels=None, borders=True):
-    for i, (mask, score) in enumerate(zip(masks, scores)):
-        plt.figure(figsize=(10, 10))
-        plt.imshow(image)
-        show_mask(mask, plt.gca(), borders=borders)
-        if point_coords is not None:
-            assert input_labels is not None
-            show_points(point_coords, input_labels, plt.gca())
-        if box_coords is not None:
-            # boxes
-            show_box(box_coords, plt.gca())
-        if len(scores) > 1:
-            plt.title(f"Mask {i+1}, Score: {score:.3f}", fontsize=18)
-        plt.axis('off')
-        plt.show()
-
-image = Image.open('test-image.jpg')
-image = np.array(image.convert("RGB"))
-plt.figure(figsize=(10, 10))
-plt.imshow(image)
-plt.axis('on')
-plt.show()
-
-# First, load the SAM 2 model and predictor. Change the path below to point to the SAM 2 checkpoint. 
-# Running on CUDA and using the default model are recommended for best results.
 sam2_checkpoint = "./checkpoints/sam2_hiera_large.pt"
 model_cfg = "sam2_hiera_l.yaml"
 sam2_model = build_sam2(model_cfg, sam2_checkpoint, device="cuda")
 predictor = SAM2ImagePredictor(sam2_model)
-# Process the image to produce an image embedding by calling SAM2ImagePredictor.set_image. 
-# SAM2ImagePredictor remembers this embedding and will use it for subsequent mask prediction.
+
+# Load the image
+image = cv2.imread('example-1.png')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 predictor.set_image(image)
 
-# To select the truck, choose a point on it. Points are input to the model in (x,y) format and come with labels 1 (foreground point) or 0 (background point). 
-# Multiple points can be input; here we use only one. The chosen point will be shown as a star on the image.
-input_point = np.array([[500, 375]])
-input_label = np.array([1])
+# coordinates
+input_points = np.array([[577, 355]])
+input_labels = np.array([1])  # Assuming all points are foreground
 
-plt.figure(figsize=(10, 10))
-plt.imshow(image)
-show_points(input_point, input_label, plt.gca())
-plt.axis('on')
-plt.show()
+# Generate the mask
+masks, _, _ = predictor.predict(point_coords=input_points, point_labels=input_labels, multimask_output=False)
+# save all masks
+#for i, mask in enumerate(masks):
+#    cv2.imwrite(f'output_mask_{i}.png', mask * 255)
 
+
+# Select the best mask (typically the last one)
+#best_mask = masks[-1]
+
+# Define your RGBA color (e.g., red with 50% transparency)
+#rgba_color = [255, 0, 0, 128]  # Red with 50% opacity
+
+# Create an empty RGBA image
+#rgba_image = np.zeros((image.shape[0], image.shape[1], 4), dtype=np.uint8)
+
+# Apply the RGBA color to the selected area using the mask
+#rgba_image[best_mask == 1] = rgba_color
+
+# Save the RGBA image
+#cv2.imwrite('output_rgba_image.png', rgba_image)
---
+++ predict.py
@@ -0,0 +1,51 @@
+# Prediction interface for Cog ⚙️
+# https://cog.run/python
+
+from cog import BasePredictor, Input, Path
+from sam2.build_sam import build_sam2
+from sam2.sam2_image_predictor import SAM2ImagePredictor
+import torch
+import cv2
+import numpy as np
+import json
+
+class Predictor(BasePredictor):
+    def __init__(self):
+        self.predictor = None
+
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        sam2_checkpoint = "./checkpoints/sam2_hiera_large.pt"
+        model_cfg = "sam2_hiera_l.yaml"
+        sam2_model = build_sam2(model_cfg, sam2_checkpoint, device="cuda")
+        self.predictor = SAM2ImagePredictor(sam2_model)
+
+    def predict(
+        self,
+        image: Path = Input(description="Input image"),
+        points: Path = Input(description="Input points")
+    ) -> Path:
+        """Run a single prediction on the model"""
+        
+        # Load the image
+        image = cv2.imread(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        self.predictor.set_image(image)
+
+        point_list = json.loads(points)
+        input_points = []
+        input_labels = []
+        for point in point_list:
+            input_points.append(point['coordinate'])
+            input_labels.append(point['type'])
+        input_point = np.array(input_points)
+        input_label = np.array(input_labels)
+
+        masks, _, _ = self.predictor.predict(
+            point_coords=input_point,
+            point_labels=input_label,
+            multimask_output=False,
+        )
+        best_mask = masks[-1]
+        cv2.imwrite(f'output_mask.png', best_mask * 255)
+        return Path("output_mask.png")
